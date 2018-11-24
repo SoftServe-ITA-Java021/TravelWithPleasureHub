c5d525f8299b001112fd21b28a2133ec7202506b
Yegor Fisher <egorfisher88@gmail.com>
2018-11-24 12:25:02 +0200

    commiting test project

---
+++ TravelWithPleasureHub/src/main/java/com/kh021j/travelwithpleasurehub/TravelwithpleasurehubApplication.java
@@ -0,0 +1,12 @@
+package com.kh021j.travelwithpleasurehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelwithpleasurehubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelwithpleasurehubApplication.class, args);
+    }
+}
