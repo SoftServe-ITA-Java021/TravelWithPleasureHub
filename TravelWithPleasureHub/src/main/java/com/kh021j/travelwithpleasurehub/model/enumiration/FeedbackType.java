e6cded456cba315b6d15e7717368603c0e2a1a89
Bogdan Ataskhiev <jumper-2017@mail.ru>
2019-01-02 21:47:02 +0300

    Debug

--- TravelWithPleasureHub/src/main/java/com/kh021j/travelwithpleasurehub/model/enumiration/FeedbackType.java
+++
@@ -1,5 +0,0 @@
-package com.kh021j.travelwithpleasurehub.model.enumiration;
-
-public enum FeedbackType {
-  POSITIVE, NEUTRAL, NEGATIVE
-}
--- TravelWithPleasureHub/src/main/java/com/kh021j/travelwithpleasurehub/model/enumiration/MeetingType.java
+++
@@ -1,5 +0,0 @@
-package com.kh021j.travelwithpleasurehub.model.enumiration;
-
-public enum MeetingType {
-    CINEMA, WALKING, CONCERT, FOOTBALL_MATCH
-}
