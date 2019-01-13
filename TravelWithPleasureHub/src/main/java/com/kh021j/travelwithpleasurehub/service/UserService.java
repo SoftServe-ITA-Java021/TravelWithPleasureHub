53d6ef90f9802fdc9e157090f9eddd1191cc1d10
RomanPeresada <roman.peresada@nure.ua>
2019-01-13 12:37:26 +0200

    Fix port

--- TravelWithPleasureHub/src/main/java/com/kh021j/travelwithpleasurehub/service/UserService.java
+++ TravelWithPleasureHub/src/main/java/com/kh021j/travelwithpleasurehub/service/UserService.java
@@ -1,38 +1,17 @@
 package com.kh021j.travelwithpleasurehub.service;
 
 import com.kh021j.travelwithpleasurehub.model.User;
-import com.kh021j.travelwithpleasurehub.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
-@Service
-public class UserService {
-
-    @Autowired
-    private UserRepository userRepository;
-
-    public List<User> findAll() {
-        return userRepository.findAll();
-    }
-
-    public List<User> findUserByName(String name) {
-        return userRepository.findUsersByUsername(name);
-    }
-
-    @Transactional
-    public User create(User user) {
-        return userRepository.save(user);
-    }
-
-    public Optional<User> getById(Integer id) {
-        if (userRepository.existsById(id)){
-            return Optional.of(userRepository.getOne(id));
-        }
-        return Optional.empty();
-    }
-
+public interface UserService {
+    List<User> findAll();
+    List<User> findUserByName(String username);
+    User create (User user);
+    User update(User user);
+    User getUser(int id);
+    User getUser(String email);
+    void blockUser(int id);
+    Optional<User> getById(Integer id);
 }
