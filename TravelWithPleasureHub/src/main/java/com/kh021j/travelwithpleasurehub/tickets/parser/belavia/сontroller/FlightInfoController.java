7c5c70ff24849495b4a88b555f4233e3a0754e43
musicsupermax <musicsupermax@gmail.com>
2019-01-16 15:09:06 +0200

    fixes

--- TravelWithPleasureHub/src/main/java/com/kh021j/travelwithpleasurehub/tickets/parser/belavia/сontroller/FlightInfoController.java
+++ TravelWithPleasureHub/src/main/java/com/kh021j/travelwithpleasurehub/tickets/parser/belavia/сontroller/FlightInfoController.java
@@ -1,19 +1,30 @@
+
 package com.kh021j.travelwithpleasurehub.tickets.parser.belavia.сontroller;
+
 
 import com.kh021j.travelwithpleasurehub.tickets.parser.belavia.model.BelaviaJson;
 import com.kh021j.travelwithpleasurehub.tickets.parser.belavia.model.FlightInfo;
-import com.kh021j.travelwithpleasurehub.tickets.parser.belavia.service.FlightInfoService;
+import com.kh021j.travelwithpleasurehub.tickets.parser.belavia.service.JsonConverter;
+import com.kh021j.travelwithpleasurehub.tickets.parser.belavia.service.JsonService;
 import org.springframework.web.bind.annotation.*;
-
 import java.io.IOException;
 
 @RestController
-@RequestMapping("flights")
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("api/flights")
 public class FlightInfoController {
 
     @ResponseBody
     @PostMapping
     public FlightInfo getFlightInfo(@RequestBody BelaviaJson belaviaJson) throws IOException {
-        return new FlightInfoService().createFlightInfo(belaviaJson);
+        String jsonQuery = new JsonConverter().objectToJson(belaviaJson);
+        JsonService jsonService = new JsonService();
+        String jsonBelavia = jsonService.getJsonResponse(jsonQuery);
+
+        return new FlightInfo(jsonService.getMinPriceFromResponse(jsonBelavia),
+                belaviaJson.getCurrency(), jsonService.getDepartureDateTime(jsonBelavia),
+                jsonService.getArrivalDateTime(jsonBelavia), jsonService.getDuration(jsonBelavia),
+                belaviaJson.getSearchRoutes().get(0).getOrigin(),
+                belaviaJson.getSearchRoutes().get(0).getDestination());
     }
 }
